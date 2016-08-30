fbb40072fd7bd39acd0597e595df2941f469d746
M. Hofman <m.hofman@chronos-technology.nl>
2016-08-30 17:02:48 +0200

    Add error catch to http endpoint

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,11 @@ var relationService = require('./services/relation');
 router.get("/relationship", (req, res, next) => {
     relationService.getRelation()
                    .then(x => res.json(x))
+                   .catch(error => {
+                       console.log(error);
+
+                       res.sendStatus(500);
+                   })
 });
 
 module.exports = router;
