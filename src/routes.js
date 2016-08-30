167873a74836c71b8d1f6cae40368f89d5c9e05e
M. Hofman <m.hofman@chronos-technology.nl>
2016-08-30 16:51:21 +0200

    Add call to relation in http  endpoint

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var relationService = require('./services/relation');
 
 router.get("/relationship", (req, res, next) => {
-
+    relationService.getRelation()
+                   .then(x => res.json(x))
 });
 
 module.exports = router;
--- src/services/relation.js
+++ src/services/relation.js
@@ -2,13 +2,19 @@ module.exports = {
     getRelation: getRelations
 };
 
+
+var fs = require('fs');
 var config = require("config");
 var relationProvider = require('relation-provider');
 
 relationProvider.setLocationProvider("kubernetes", {
-    master: config.get('kubernetes-master')
+    master: config.get('kubernetes-master'),
+    caFile: config.get('caFile'),
+    auth: {
+        type: "token",
+        token: fs.readFileSync(config.get('tokenFile'))
+    }
 });
-
 
 var relations = relationProvider.getRelations();
 
