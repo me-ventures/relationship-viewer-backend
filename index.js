cfcf54e5122a44da7b78a4d49d2fc8ff07c255ed
EvaLok <squidstock@hotmail.com>
2016-09-01 11:06:17 -0600

    allow cors

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ var config = require('config');
 var express = require('express');
 var app = express();
 
+
+
 app.listen(config.get('port'));
+
+// allow cors
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
+
 app.use('/', require('./src/routes'));
